8d8b44f31833408586805e50621a5603e59ebb76
Ryan A. Pavlik <ryan.pavlik@gmail.com>
2019-06-17 10:59:18 -0500

    Set up CI with Azure Pipelines

---
+++ .azure-pipelines/install_vulkan.ps1
@@ -0,0 +1,26 @@
+if (-not $env:VULKAN_SDK_VERSION) {
+    $env:VULKAN_SDK_VERSION = "1.1.106.0"
+}
+
+$SDK_VER = $env:VULKAN_SDK_VERSION
+
+if (-not (Test-Path env:VULKAN_SDK)) {
+    if ($env:SYSTEM_DEFAULTWORKINGDIRECTORY) {
+        $env:VULKAN_SDK = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY\vulkan_sdk\$SDK_VER"
+    } else {
+        $env:VULKAN_SDK = "c:\VulkanSDK\$SDK_VER"
+    }
+}
+Write-Output "Trying for Vulkan SDK $SDK_VER"
+$FN = "VulkanSDK-$SDK_VER-Installer.exe"
+$URL = "https://sdk.lunarg.com/sdk/download/$SDK_VER/windows/$FN"
+if (-not (Test-Path "$env:VULKAN_SDK/Include/vulkan/vulkan.h")) {
+    Write-Output "Downloading $URL"
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($URL, "$(pwd)\$FN")
+
+    Write-Output "Running $FN in silent, blocking mode - installing to $env:VULKAN_SDK"
+    Start-Process $FN -Wait -ArgumentList "/S", "/D=$env:VULKAN_SDK"
+} else {
+    Write-Output "$env:VULKAN_SDK found and contains header"
+}
--- maintainer-scripts/common.sh
+++ maintainer-scripts/common.sh
@@ -169,6 +169,7 @@ getSDKFilenames() {
         src/external/CMakeLists.txt \
         src/external/jsoncpp \
         src/.clang-format \
+        .azure_pipelines \
         | grep -v "gfxwrapper" \
         | grep -v "include/.gitignore" \
         | grep -v "images"

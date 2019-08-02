bec56b96551f8b3b1792b3534f1e95ef6e1dee0c
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-08-02 11:07:35 -0500

    Add script to reflow loader docs.

---
+++ specification/loader/reflow_loader.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Copyright (c) 2019 The Khronos Group Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Reflows the loader docs in-place.
+set -e
+(
+    cd "$(dirname $0)"
+    if git diff-files --quiet; then
+        # No unstaged changes
+        ../scripts/reflow.py -overwrite ./*.adoc
+    else
+        echo "Will not reflow - there are unstaged changes in this repo!"
+    fi
+)

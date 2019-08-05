1e6ba6581190b5221660f33f557aaca49e1791a8
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-08-05 17:06:39 -0500

    loader: Remove unnecessary extern "C"

--- src/loader/loader_core.cpp
+++ src/loader/loader_core.cpp
@@ -63,8 +63,6 @@ inline bool IsMissingNullTerminator(const char (&str)[max_length]) {
     return true;
 }
 
-extern "C" {
-
 // ---- Core 1.0 manual loader trampoline functions
 
 LOADER_EXPORT XRAPI_ATTR XrResult XRAPI_CALL xrEnumerateApiLayerProperties(uint32_t propertyCapacityInput,
@@ -645,5 +643,3 @@ XRAPI_ATTR XrResult XRAPI_CALL xrSessionInsertDebugUtilsLabelEXT(XrSession sessi
     return XR_SUCCESS;
 }
 XRLOADER_ABI_CATCH_FALLBACK
-
-}  // extern "C"

d7738f24ec0a343a54827aa96e9cfa63e9a424fe
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-08-27 09:36:41 -0500

    adjust vulkan install

--- .azure-pipelines/install_vulkan.ps1
+++ .azure-pipelines/install_vulkan.ps1
@@ -5,13 +5,20 @@ if (-not $env:VULKAN_SDK_VERSION) {
 $SDK_VER = $env:VULKAN_SDK_VERSION
 
 if (-not (Test-Path env:VULKAN_SDK)) {
-    $env:VULKAN_SDK = "c:\VulkanSDK\$SDK_VER"
+    if ($env:SYSTEM_DEFAULTWORKINGDIRECTORY) {
+        $env:VULKAN_SDK = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY\vulkan_sdk\$SDK_VER"
+    } else {
+        $env:VULKAN_SDK = "c:\VulkanSDK\$SDK_VER"
+    }
 }
-
+Write-Output "Trying for Vulkan SDK $SDK_VER"
 $FN = "VulkanSDK-$SDK_VER-Installer.exe"
 $URL = "https://sdk.lunarg.com/sdk/download/$SDK_VER/windows/$FN"
 if (-not (Test-Path "$env:VULKAN_SDK/Include/vulkan/vulkan.h")) {
+    Write-Output "Downloading $URL"
     $wc = New-Object System.Net.WebClient
     $wc.DownloadFile($URL, $FN)
-    Start-Process $FN -ArgumentList "/S" -Wait
+
+    Write-Output "Running $FN in silent, blocking mode"
+    Start-Process $FN -ArgumentList "/S" -Wait "/D=$env:VULKAN_SDK"
 }
